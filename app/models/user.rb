6c9fe230594d5c69f9cf2a4a277df18a98b84c60
jdoleiv <jdoleiv@gmail.com>
2017-06-05 15:51:16 +0800

    Added all models and views through order mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  after_destroy :ensure_an_admin_remains
+
+  validates :name, presence: true, uniqueness: true
+  has_secure_password
+
+  private
+    def ensure_an_admin_remains
+      if User.count.zero?
+        raise Error.new "Can't delete last user"
+      end
+    end
 end
